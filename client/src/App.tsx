23d746b4715821e0c1322ff00ec178d943c63e19
lukrai <lukrai007@gmail.com>
2019-10-01 17:13:57 +0300

    BrowserRouter, Home, Register, Login pages, initial Register form and logic.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,32 @@
 import React from "react";
-import { useHelloQuery } from "./generated/graphql";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 const App: React.FC = () => {
-  const { data, loading } = useHelloQuery();
-  if (loading || !data) {
-    return <div>Loading...</div>;
-  }
-  return <div>{data.hello}</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/register">Register</Link>
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+          </div>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
