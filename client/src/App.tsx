79c82458f5f0ade16ae2d118cbfa54e70c40d8f9
lukrai <lukrai007@gmail.com>
2019-10-01 12:39:51 +0300

    Initial react setup and graphql hello query.

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const App: React.FC = () => {
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export default App;
